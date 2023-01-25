42bc3f9e69085a3a4e6afa1e538eeacf8ecdccad
Adam <63548917+itsaderm@users.noreply.github.com>
2023-01-25 23:10:15 +0000

    Optimizations & Fix duplicate images

--- scraper.py
+++ scraper.py
@@ -1,7 +1,14 @@
+import os
 import praw # library for interacting with the Reddit API
 import requests # library for sending HTTP requests
 import random # library for generating random numbers
 from concurrent.futures import ProcessPoolExecutor # library for running multiple threads in parallel
+import json # library for working with JSON data
+
+# Check if the posts.json file exists, if not create it
+if not os.path.exists("posts.json"):
+    with open("posts.json", "w") as f:
+        json.dump({}, f)
 
 # create a Reddit object with client_id, client_secret, and user_agent
 reddit = praw.Reddit(
@@ -11,7 +18,10 @@ reddit = praw.Reddit(
 )
 # minimum number of upvotes for a post to be considered valid
 likeCount = 700
- 
+
+# Create a dictionary to store post title & author name as key and direct_image_url as value
+data = {}
+
 def scanReddit(name):
     """
     Scans subreddit for valid image posts.
@@ -33,19 +43,19 @@ def scanReddit(name):
                     for image in images:
                         direct_image_url = image["source"]["url"]
                         # Create a string of the post title & author name.
-                        titleauthor = (submission.title + submission.author.name).lower().replace(" ", "") + "\n"
-                        titleauthor_bytes = titleauthor.encode('utf-8')
-                        with open("posts.txt", "a+") as f:
-                            data = f.read()
-                            # check if the post title & author name already exists in the text file
-                            if data.find(titleauthor) == -1:
-                                f.write(titleauthor)
-                                # Download the image
-                                response = requests.get(direct_image_url)
-                                with open("images/" + submission.title + ".webp", "wb") as f:
-                                    f.write(response.content)
-                            else:
-                                print("Post data already exists.")
+                        titleauthor = (submission.title + submission.author.name).lower().replace(" ", "")
+                        # Check if the post title & author name already exists in the data dictionary
+                        if titleauthor not in data:
+                            data[titleauthor] = direct_image_url
+                            # dump data to the file
+                            with open("posts.json", "w") as f:
+                                json.dump(data, f)
+                            # Download the image
+                            response = requests.get(direct_image_url)
+                            with open("images/" + submission.title + ".webp", "wb") as f:
+                                f.write(response.content)
+                        else:
+                            print("Post data already exists.")
                 else:
                     print("Post is not an image post.")
             else:
@@ -62,3 +72,7 @@ with ProcessPoolExecutor(max_workers=4) as executor:
     for subreddit in subreddit_list:
         # submit the scanReddit function with the subreddit to the executor
         executor.submit(scanReddit, subreddit)
+
+# the script will continue running indefinitely
+while True:
+    continue
